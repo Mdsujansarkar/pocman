a7b33581ab06b6c2b21563f24b0e4bfb633d01be
Mdsujansarkar <sujansarkar786@gmail.com>
2020-11-29 17:30:26 +0600

    problem solve

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apllo/react-hooks';
+import { ApolloProvider } from '@apollo/client';
 function App() {
   const client = new ApolloClient({
-    uri: ''
+    uri: 'https://graphql-pokemon.sh/'
   });
   return (
-    <div className="App">
-      <header className="App-header">
+    <ApolloProvider client={client}>
+    <main>
     <p>hello</p>
-      </header>
-    </div>
+    </main>
+    </ApolloProvider>
   );
 }
 
